82d409d22640298fa6d4c207565e8458070726c3
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-03-13 17:01:23 +0100

    FirstCommit :)

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique()->required();
+            $table->string('firstName')->required();
+            $table->string('lastName')->required();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
