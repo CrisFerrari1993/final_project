c66799fb19f550cb145a7ea295b3960482e84162
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-03-14 15:06:33 +0100

    add store method

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -12,21 +12,50 @@ use App\Models\Order;
 
 class RestaurantController extends Controller
 {
-    public function index(){
-        $restaurants = Restaurant :: all();
-        $users = User :: all();
+    public function index()
+    {
+        $restaurants = Restaurant::all();
+        $users = User::all();
 
         return view('restaurant.index', compact('restaurants', 'users'));
     }
 
-    public function create(){
+    public function create()
+    {
 
-        $restaurants = Restaurant :: all();
-        $categories = Category :: all();
-        $dishes = Dish :: all();
+        $restaurants = Restaurant::all();
+        $categories = Category::all();
+        $dishes = Dish::all();
+        $users = User::all();
 
-        return view('restaurant.create', compact('restaurants', 'categories', 'dishes'));
+        return view('restaurant.create', compact('restaurants', 'categories', 'dishes', 'users'));
 
+
+    }
+
+    public function store(Request $request)
+    {
+
+        $data = $request->all();
+
+        $user = $request->user();
+
+        $newRestaurant = new Restaurant();
+
+        $newRestaurant->name = $data['name'];
+        $newRestaurant->adress = $data['adress'];
+        $newRestaurant->adress = $data['adress'];
+        $newRestaurant->user()->associate($user);
+
+        // capire come popolare le colonne contenenti immagini (logo e wallpaper)
+
+        $newRestaurant->vat_num = $data['vat_num'];
+
+        $newRestaurant->save();
+
+        $newRestaurant->categories()->attach($data['category_id']);
+
+        return redirect()->route('restaurant.index');
 
     }
 }
