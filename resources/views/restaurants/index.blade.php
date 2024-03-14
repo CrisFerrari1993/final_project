81ccb622d8a045afc1faa2d1652d7b63ce0460c8
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-03-14 11:17:06 +0100

    Auth: dashboard show only logghed(id) associate restaurant

--- resources/views/restaurants/index.blade.php
+++ resources/views/restaurants/index.blade.php
@@ -1,13 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
+    
     <ul>
         @foreach ($restaurants as $restaurant)
-            <li>
-                <h4>{{$restaurant->name}}</h4>
-                <img src="{{$restaurant->wallpaper}}" alt="">
-                <span>{{$restaurant->user->lastName}}</span>
-            </li>
+            @auth
+                @if (auth()->user()-> id == $restaurant->user_id)
+                    <li>
+                        <h4>{{$restaurant->name}}</h4>
+                        <img src="{{$restaurant->wallpaper}}" alt="">
+                        <span>{{$restaurant->user->lastName}}</span>
+                    </li>
+                @endif
+            @endauth    
         @endforeach
     </ul>
 @endsection
