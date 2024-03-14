fccd35f8705fca59505e143dce725fa636f00be4
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-03-14 15:16:48 +0100

    use storage

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Storage;
+
 use App\Models\Restaurant;
 use App\Models\User;
 use App\Models\Category;
