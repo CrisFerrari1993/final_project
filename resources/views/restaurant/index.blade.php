5a97c300ec6fd55deda093b8665439e327af3765
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-03-14 18:35:55 +0100

    Recreate index page

--- resources/views/restaurant/index.blade.php
+++ resources/views/restaurant/index.blade.php
@@ -2,39 +2,20 @@
 
 @section('content')
 @if (auth()->user()->restaurant)
+    <img src="{{auth()->user()->restaurant ->wallpaper}}" alt="">
+    <h1>{{auth()->user()->restaurant->name}}</h1>
     <div class="text-center m-4">
         <button class="btn btn-primary"><a class="text-white" href="{{route('dish.create')}}"> + Add dish</a></button>
     </div>
     <ul>
-        <!-- delete foreach -->
-        @foreach ($restaurants as $restaurant)
-            @auth
-                @if (auth()->user()->id == $restaurant->user_id)
-                    <li>
-                        <h4>{{$restaurant->name}}</h4>
-                        <strong>Indirizzo ristorante: </strong>
-                        <span>{{ $restaurant->adress }}</span>
-                        <br>
-                        <strong>Partita iva: </strong>
-                        <span>{{ $restaurant->vat_num }}</span>
-                        <br>
-                        <img src="{{$restaurant->wallpaper}}" alt="">
-                        <strong>Nome del proprietario</strong>
-                        <span>{{$restaurant->user->firstName}}</span>
-                        <br>
-                        <strong>Cognome del proprietario</strong>
-                        <span>{{$restaurant->user->lastName}}</span>
-                        <br>
-                        <strong>Logo ristorante</strong>
-                        <img src="{{ asset('storage/' . $restaurant->logo) }}" alt="logo" style="width: 100px; height: 100px">
-                        <br>
-                        <strong>Wallpaper ristorante</strong>
-                        <img src="{{ asset('storage/' . $restaurant->wallpaper) }}" alt="wallpapaer" style="width: 100px; height:100px;">
-                    </li>
-                @endif
-            @endauth    
+        @foreach (auth()->user()->restaurant->dishes as $dish)
+            <li>
+                <h1>{{$dish->name}}</h1>
+                <img src="{{asset('storage/' . $dish->image)}}" alt="">
+            </li>
         @endforeach
     </ul>
+
 @else
 <div class="text-center m-4">
     <button class="btn btn-primary"><a class="text-white" href="{{route('restaurant.create')}}"> + Add restaurant</a></button>
