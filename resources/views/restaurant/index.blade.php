1204d3f35f57f45f72e0a2e33e80d8840b3bea8c
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-03-14 18:48:00 +0100

    Dish in index

--- resources/views/restaurant/index.blade.php
+++ resources/views/restaurant/index.blade.php
@@ -7,15 +7,18 @@
     <div class="text-center m-4">
         <button class="btn btn-primary"><a class="text-white" href="{{route('dish.create')}}"> + Add dish</a></button>
     </div>
-    <ul>
-        @foreach (auth()->user()->restaurant->dishes as $dish)
-            <li>
-                <h1>{{$dish->name}}</h1>
-                <img src="{{asset('storage/' . $dish->image)}}" alt="">
-            </li>
-        @endforeach
-    </ul>
-
+    <div class="container">
+        <div class="row">
+            @foreach (auth()->user()->restaurant->dishes as $dish)
+                <div class="p-2 col-sm-12 col-md-6 col-xl-3">
+                    <a class="idx_link" href="">
+                        <h5>{{$dish->name}}</h5>
+                        <img class="card" src="{{asset('storage/' . $dish->image)}}" alt="">
+                    </a>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @else
 <div class="text-center m-4">
     <button class="btn btn-primary"><a class="text-white" href="{{route('restaurant.create')}}"> + Add restaurant</a></button>
