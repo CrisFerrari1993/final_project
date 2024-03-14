ae70549bc32202be79b8a3b7ce1fd87e79a68316
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-03-14 09:50:44 +0100

    Index route done

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,15 @@ use App\Http\Controllers\RestaurantController;
 */
 
 
-Route::get('/',[RestaurantController :: class, 'index'])
-    ->name('restaurants.index');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/',[RestaurantController :: class, 'index'])
+    ->name('restaurants.index');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
