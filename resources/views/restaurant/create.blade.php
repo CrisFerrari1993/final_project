a3b6f6527b189cf4a71245c3d5d52b813b517890
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-03-14 15:16:01 +0100

    add enctype to the form

--- resources/views/restaurant/create.blade.php
+++ resources/views/restaurant/create.blade.php
@@ -5,7 +5,7 @@
     <h1>Create new restaurant</h1>
 
     <!-- form -->
-    <form action="{{ route('restaurant.store') }}" method="POST">
+    <form action="{{ route('restaurant.store') }}" method="POST" enctype="multipart/form-data">
 
     @csrf
     @method('POST')
