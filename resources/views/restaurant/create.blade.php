3d517de53d14ee909bfede8171d0c6287c8b3020
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-03-14 14:42:02 +0100

    add form to create restaurant

--- resources/views/restaurant/create.blade.php
+++ resources/views/restaurant/create.blade.php
@@ -1,4 +1,67 @@
 @extends('layouts.app')
 @section('content')
-    <h1>Create</h1>
+
+    <!--  title -->
+    <h1>Create new restaurant</h1>
+
+    <!-- form -->
+    <form method="POST">
+
+    @csrf
+    @method('POST')
+
+    <!-- input restaurant name -->
+    <label for="name">Nome ristorante</label>
+    <input type="text" name="name" id="name">
+
+    <br>
+    <br>
+
+    <!-- input restaurant adress -->
+    <label for="adress">Indirizzo ristorante</label>
+    <input type="text" name="adress" id="adress">
+
+    <br>
+    <br>
+    <!-- input restaurant logo -->
+    <!-- !!!!! AGGIUNGERE  PASSAGGIO FILE STORAGE !!!!! -->
+    <label for="logo">Logo ristorante</label>
+    <input type="file" name="logo" id="logo">
+
+    <br>
+    <br>
+
+    <!-- input restaurant wallpaper -->
+    <!-- !!!!! AGGIUNGERE  PASSAGGIO FILE STORAGE !!!!! -->
+    <label for="wallpaper">Wallpaper ristorante</label>
+    <input type="file" name="wallpaper" id="wallpaper">
+
+    <br>
+    <br>
+
+    <!-- input restaurant vat num -->
+    <label for="vat_num">P.iva ristorante</label>
+    <input type="text" name="vat_num" id="vat_num">
+    
+    <br>
+    <br>
+
+    <!-- input for choose category -->
+    <label for="category">Categoria ristorante</label>
+    @foreach ($categories as $category)
+        <input
+            type="checkbox"
+            name="category_id[]"
+            id="{{ 'category_id_' . $category->id }}"
+            value="{{ $category->id }}"
+        >
+        <label
+            for="{{ 'category_id_' . $category->id }}">
+            {{ $category->name }}
+        </label>
+        <br>
+    @endforeach
+
+    </form>
+
 @endsection
