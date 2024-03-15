05b833dbf3c6309dc1931c1145e5be50c1f4b765
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-03-15 11:44:05 +0100

    show error message

--- app/Http/Controllers/DishController.php
+++ app/Http/Controllers/DishController.php
@@ -104,7 +104,7 @@ class DishController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(DishFormRequest $request, $id)
     {
 
         $data = $request->all();
