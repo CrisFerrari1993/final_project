a33aefbf5f725dbc49592b26fc16e80d7d65d802
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-03-18 15:00:15 +0100

    model import

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,6 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+// importazione dei model
+use App\Models\Restaurant;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Dish;
+use App\Models\Order;
+
 class ApiController extends Controller
 {
     //
