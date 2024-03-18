c4c882b642740baf4e29c2bbf2ba2b32596b2bb6
SergioBuzi <sergio.buzi96@gmail.com>
2024-03-18 22:03:22 +0100

    fix: auth in restaurant edit

--- resources/views/restaurant/edit-restaurant.blade.php
+++ resources/views/restaurant/edit-restaurant.blade.php
@@ -1,35 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{$restaurant->name}}</h1>
 
-    
-    
-    <h3>Questa è la sezione in cui puoi cambiare la visibilità del tuo ristorante</h3>
-    <h4>Scegli se essere visibile ai clienti, oppure invisibile</h4>
+    @if (Auth::check() && Auth::id() === $restaurant->user_id)
+        @auth
+            <h1>{{$restaurant->name}}</h1>
 
-    <form method="POST">
-        @csrf
-        @method('PUT')
+            
+            
+            <h3>Questa è la sezione in cui puoi cambiare la visibilità del tuo ristorante</h3>
+            <h4>Scegli se essere visibile ai clienti, oppure invisibile</h4>
 
-        <div class="mb-4 row">
-            <label class="form-check-label" for="flexRadioDefault1">
-                Visibile
-            <input class="form-check-input mx-1" {{$restaurant->visibility === 1 ? 'checked' : ''}} type="radio" name="visibility" id="visibility" value="1">
+            <form method="POST">
+                @csrf
+                @method('PUT')
 
-            <label class="form-check-label" for="flexRadioDefault1">
-                Non Visibile
-            <input class="form-check-input mx-1" {{$restaurant->visibility === 0 ? 'checked' : ''}} type="radio" name="visibility" id="visibility" value="0"><br>
-        </div>
+                <div class="mb-4 row">
+                    <label class="form-check-label" for="flexRadioDefault1">
+                        Visibile
+                    <input class="form-check-input mx-1" {{$restaurant->visibility === 1 ? 'checked' : ''}} type="radio" name="visibility" id="visibility" value="1">
 
-        <div class="mb-4 w-100 mb-0">
-            <div>
-                <button type="submit" class="btn btn-primary">
-                    {{ __('Conferma cambiamenti') }}
-                </button>
-            </div>
-        </div>
+                    <label class="form-check-label" for="flexRadioDefault1">
+                        Non Visibile
+                    <input class="form-check-input mx-1" {{$restaurant->visibility === 0 ? 'checked' : ''}} type="radio" name="visibility" id="visibility" value="0"><br>
+                </div>
 
-    </form>
+                <div class="mb-4 w-100 mb-0">
+                    <div>
+                        <button type="submit" class="btn btn-primary">
+                            {{ __('Conferma cambiamenti') }}
+                        </button>
+                    </div>
+                </div>
+
+            </form>
+        @endauth
+        
+    @else
+        <h1>Ops... Qualcuno sta provando a fare l'hackerino</h1>
+    @endif
 
 @endsection
