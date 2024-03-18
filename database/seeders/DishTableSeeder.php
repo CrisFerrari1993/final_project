17fa108e2ac15029b783e4ac7b2bbcceb8aa084d
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-03-18 19:09:17 +0100

    import model

--- database/seeders/DishTableSeeder.php
+++ database/seeders/DishTableSeeder.php
@@ -5,6 +5,10 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+// import model
+use App\Models\Dish;
+use App\Models\Restaurant;
+
 class DishTableSeeder extends Seeder
 {
     /**
