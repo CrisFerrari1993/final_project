1dc9932a5684c127d2e591e93644dd268621fcf9
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-03-20 17:20:05 +0100

    init commit payment

--- resources/js/app.js
+++
@@ -1,4 +0,0 @@
-import "./bootstrap";
-import "~resources/scss/dashboard.scss";
-import * as bootstrap from "bootstrap";
-import.meta.glob(["../img/**"]);
--- resources/views/welcome.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-@section('content')
-<div id="wrapper">
-    <h1>Welcome</h1>
-</div>
-
-@endsection
