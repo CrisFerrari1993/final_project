62e97bbec6c1689b39c8e593172042d02bf8cffc
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-04-02 13:10:21 +0200

    test

--- resources/views/restaurant/index.blade.php
+++ resources/views/restaurant/index.blade.php
@@ -2,7 +2,58 @@
 
 @section('content')
     @if (auth()->user()->restaurant)
-        <div class="card mb-3" style="border: none">
+
+    <div class="intro">
+
+        <div class="container pt-5">
+
+            <div class="row d-flex justify-content-between align-items-center">
+
+                <div class="col">
+                    <img class="img-thumbnail" src="{{ asset('storage/' . auth()->user()->restaurant->logo) }}" alt="logo">
+                </div>
+
+                <div class="col bottoni">
+                    <h1 class="card-title mt-5"><strong>{{ auth()->user()->restaurant->name }}</strong></h1>
+                    <h3 class="card-text m-0"><i class="fa-solid fa-location-dot"></i>
+                        {{ auth()->user()->restaurant->adress }}</h3>
+                    <h4 class="card-text"><i class="fa-solid fa-cash-register"></i> P.IVA
+                        {{ auth()->user()->restaurant->vat_num }}</h4>
+                    <div>
+                        @foreach ($restaurant->categories as $item)
+                            <h4 class="badge rounded-pill bg-dark text-white">{{ $item->name }}</h4>
+                        @endforeach
+                    </div>
+                </div>
+
+                <div class="col">
+                    @if (auth()->user()->restaurant->visibility)
+                        {{-- se visibility è vero --}}
+                        <button class="btn btn-danger mb-2">
+                            <a class="btn_link" href="{{ route('restaurant.edit', $restaurant->id) }}">Prenditi una
+                                pausa</a>
+                        </button>
+                    @else
+                        <button class="btn btn-success mb-2">
+                            <a class="btn_link" href="{{ route('restaurant.edit', $restaurant->id) }}">Torna a
+                                lavoro</a>
+                        </button>
+                    @endif
+                    {{-- pulsante per la pagina con gli ordini --}}
+                    <button class="btn btn-primary me-2" style="background-color: #ffc244; border:none;">
+                        <a class="btn_link" href="{{ route('order.show', $restaurant->id) }}">
+                            Visualizza gli ordini
+                        </a>
+                    </button>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+        <!-- <div class="card mb-3" style="border: none">
 
             <div class="card-body s-card" >
                 <div class="row">
@@ -60,7 +111,8 @@
 
                     </div>
                 </div>
-            </div>    
+            </div>   -->
+
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 160"><path fill="#00a082" fill-opacity="1" d="M0,128L30,133.3C60,139,120,149,180,144C240,139,300,117,360,112C420,107,480,117,540,112C600,107,660,85,720,80C780,75,840,85,900,96C960,107,1020,117,1080,128C1140,139,1200,149,1260,154.7C1320,160,1380,160,1410,160L1440,160L1440,0L1410,0C1380,0,1320,0,1260,0C1200,0,1140,0,1080,0C1020,0,960,0,900,0C840,0,780,0,720,0C660,0,600,0,540,0C480,0,420,0,360,0C300,0,240,0,180,0C120,0,60,0,30,0L0,0Z"></path></svg>        </div>
 
         <div class="text-center">
@@ -106,6 +158,14 @@
 
 <style>
 
+    .intro {
+        background-color:  #00a082;
+    }
+
+    /* .bottoni {
+        display: flex;
+        flex-direction: row
+    } */
    
     .s-card {
         background-repeat: no-repeat;
